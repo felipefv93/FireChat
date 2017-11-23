3658a5cab4bf594d68337e86e2dd5e85fbec14cc
Felipe Fierro <felipefv@MacBook-Air-de-Felipe.local>
2017-11-23 16:05:53 -0700

    Instalacion de AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from "angularfire2";
+import { environment } from "../environments/environment";
+import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA8dXAW3RQGkGPLmp20ueQTqCOTSKFemRE",
+    authDomain: "heroesapp-b0185.firebaseapp.com",
+    databaseURL: "https://heroesapp-b0185.firebaseio.com",
+    projectId: "heroesapp-b0185",
+    storageBucket: "heroesapp-b0185.appspot.com",
+    messagingSenderId: "414865331845"
+  }
 };
